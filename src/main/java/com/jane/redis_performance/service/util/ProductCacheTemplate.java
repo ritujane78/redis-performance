2261841f01823cd98f63d165517fb15cba01aaf1
Ritu Bafna <ritujane78@gmail.com>
2026-04-03 17:52:38 +0545

    use local cache map instead of map

--- src/main/java/com/jane/redis_performance/service/util/ProductCacheTemplate.java
+++ src/main/java/com/jane/redis_performance/service/util/ProductCacheTemplate.java
@@ -9,7 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
-@Service
+//@Service
 public class ProductCacheTemplate extends CacheTemplate<Integer, Product>{
     @Autowired
     private ProductRepository productRepository;
